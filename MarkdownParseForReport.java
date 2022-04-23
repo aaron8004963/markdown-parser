724fd59a320a369f1de69d293c880acbdb17903d
aaron8004963 <aaron8004963@gmail.com>
2022-04-23 00:46:53 -0700

    symtom2 fixed

--- MarkdownParseForReport.java
+++ MarkdownParseForReport.java
@@ -17,12 +17,19 @@ public class MarkdownParseForReport {
             if(openBracket == -1){
                 break;
             }
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
-            
+            else{
+                
+                if(markdown.charAt(openBracket - 1) == '!'){
+                    currentIndex = openBracket + 1;
+                    continue;
+                }
+                
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+                currentIndex = closeParen + 1;
+            }
         }
 
         return toReturn;

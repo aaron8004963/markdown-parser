e2b6f86306304944582e92bd434f48682f3e140b
Aaron Li <aaron8004963@gmail.com>
2022-04-22 18:54:58 -0700

    test file (wrong version)

---
+++ MarkdownParseTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+
+public class MarkdownParseTest {
+    @Test
+    public void addition(){
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void fileTest() throws IOException{
+        ArrayList<String> toTest = new ArrayList<String>();
+        toTest.add("https://something.com");
+        toTest.add("some-thing.html");
+
+        assertEquals(MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))),
+            toTest);
+    }
+}

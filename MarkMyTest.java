569e1f5fb8c207bf78413c6f8e11f3f5660ce29e
aaron8004963 <aaron8004963@gmail.com>
2022-05-21 08:57:52 -0700

    d

--- MarkMyTest.java
+++ MarkMyTest.java
@@ -5,7 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
-public class MarkdownParseSnippetTest {
+public class MarkMyTest {
     // mark from server
     @Test 
     public void myTest() throws IOException {
@@ -43,40 +43,6 @@ public class MarkdownParseSnippetTest {
                                             Path.of("Snippet_3.md"))));
     }
 
-    @Test 
-    public void reviewedTest1() throws IOException {
-            ArrayList<String> toTest = new ArrayList<String>();
-            toTest.add("`google.com");
-            toTest.add("google.com");
-            toTest.add("ucsd.edu");
-
-            assertEquals(toTest,
-                MarkdownParseReviewed.getLinks(Files.readString(
-                                            Path.of("Snippet_1.md"))));
-    }
-
-    @Test 
-    public void reviewedTest2() throws IOException {
-            ArrayList<String> toTest = new ArrayList<String>();
-            toTest.add("a.com");
-            toTest.add("a.com(())");
-            toTest.add("example.com");
-
-            assertEquals(toTest,
-                MarkdownParseReviewed.getLinks(Files.readString(
-                                            Path.of("Snippet_2.md"))));
-    }
-
-    @Test 
-    public void reviewedTest3() throws IOException {
-            ArrayList<String> toTest = new ArrayList<String>();
-            toTest.add("https://www.twitter.com");
-            toTest.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
-            toTest.add("https://cse.ucsd.edu/");
-
-            assertEquals(toTest,
-                MarkdownParseReviewed.getLinks(Files.readString(
-                                            Path.of("Snippet_3.md"))));
-    }
+  
 
 }
